8c9b7ace0673a5332eb02415518f4f674f341760
Murat İnce <141913776+muratincee@users.noreply.github.com>
2023-09-17 23:38:46 +0300

    Add files via upload

---
+++ sos.js
@@ -0,0 +1,92 @@
+// 9 tane buton dizmelisiniz 3x3 şeklinde olmalı
+// - Basit dinamik kodlar olması yerine karışık koşullar yazsanız da problem değil. Amaç sonuca ulaşmak.
+// - Oyunda sıra sistemi olmalı. 1 oyuncu X diğer oyuncu O yapmalı
+// - Oyun 2 kişi oynuyormuş gibi kurgulanmalı
+// - Eğer yapabilirseniz bilgisayara karşı da yapabilirsiniz fakat en sona bırakın
+
+const sirag = document.querySelector('#sirag');
+const btnAll = document.querySelectorAll('.btnGm');
+
+const btn1 = document.querySelector('#btn1');
+const btn2 = document.querySelector('#btn2');
+const btn3 = document.querySelector('#btn3');
+
+const btn4 = document.querySelector('#btn4');
+const btn5 = document.querySelector('#btn5');
+const btn6 = document.querySelector('#btn6');
+
+const btn7 = document.querySelector('#btn7');
+const btn8 = document.querySelector('#btn8');
+const btn9 = document.querySelector('#btn9');
+
+
+function bindEvent() {
+
+    
+    
+    for(const btn of btnAll) {
+    
+        btn.addEventListener('click', turnFunc);
+    }
+}
+
+bindEvent();
+
+let sira = 'O';
+const winner = document.querySelector('#winner');
+
+sirag.innerHTML="Sıra =" + " " + "X";
+function turnFunc() {
+    
+    if (sira === 'X') {
+        sirag.innerHTML="Sıra =" + " " + sira;
+        this.innerText = 'O';
+        sira = 'O';
+        
+    } else {
+        sirag.innerHTML="Sıra =" + " " + sira;
+        this.innerText = 'X';
+        sira = 'X'; 
+    }
+
+    if(btn1.innerText === btn2.innerText && btn2.innerText === btn3.innerText) {
+        console.log('winner');
+        winner.innerHTML = "Kazanan= " + btn1.innerText;
+    }
+
+    if(btn4.innerText === btn5.innerText && btn5.innerText === btn6.innerText) {
+        console.log('winner');
+        winner.innerHTML = "Kazanan= " + btn4.innerText;
+    }
+
+    if(btn7.innerText === btn8.innerText && btn8.innerText === btn9.innerText) {
+        console.log('winner');
+        winner.innerHTML = "Kazanan= " + btn7.innerText;
+    }
+
+    if(btn1.innerText === btn4.innerText && btn4.innerText === btn7.innerText) {
+        console.log('winner');
+        winner.innerHTML = "Kazanan= " + btn1.innerText;
+    }
+
+    if(btn2.innerText === btn5.innerText && btn5.innerText === btn8.innerText) {
+        console.log('winner');
+        winner.innerHTML = "Kazanan= " + btn2.innerText;
+    }
+
+    if(btn3.innerText === btn6.innerText && btn6.innerText === btn9.innerText) {
+        console.log('winner');
+        winner.innerHTML = "Kazanan= " + btn3.innerText;
+    }
+
+    if(btn1.innerText === btn5.innerText && btn5.innerText === btn9.innerText) {
+        console.log('winner');
+        winner.innerHTML = "Kazanan= " + btn1.innerText;
+    }
+
+    if(btn3.innerText === btn5.innerText && btn5.innerText === btn7.innerText) {
+        console.log('winner');
+        winner.innerHTML = "Kazanan= " + btn3.innerText;
+    }
+}
+
